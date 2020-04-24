ee1fada3a94d13599039692dd930f7f92c4bdba7
joavillan <45419691+joavillan@users.noreply.github.com>
2020-04-24 03:40:24 +0200

    microservicio

---
+++ Email Micro Service/mail.py
@@ -0,0 +1,33 @@
+from flask import *
+from flask_mail import *
+
+app = Flask(__name__)
+
+app.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT=465,
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME='proyectointegradocw@gmail.com',
+    MAIL_PASSWORD='cspdirbajzsztdnx'
+)
+mail= Mail(app)
+
+@app.route("/",methods=['POST'])
+def index():
+   email = request.json['email']
+   emailId = request.json['emailId']
+   link = 'http://localhost:4200/verify/'+emailId
+   msg = Message('Verificación del correo electrónico en CookingWorld', sender = 'activate@cookingworld.com', recipients = [email])
+   msg.body = f"Pulse en el siguiente link para verificar su correo electrónico. En el caso de no haberte registrado ignore este mensaje. {link}"
+   mail.send(msg)
+   return "Sent",200
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
+if __name__ == '__main__':
+   app.run(debug = True)
